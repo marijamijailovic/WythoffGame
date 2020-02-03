d0aa996b766b7f7212827a85048914c217876842
Marija Mijailovic <mijailovicmarija@hotmail.com>
2020-02-03 14:27:53 +0100

    Init commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //Recursive Characterization of the P-Positions
+  //A_n = mex{A_i, B_i : i<n} -- mex najmanji nenegativan broj koji nije u A_n
+  //mex{} = 0
+  //
+  vector<int> A;
+  vector<int> B;
+  int n;
+  int a = 2;
+
+  cin >> n;
+  
+  A[0] = 0;
+  B[0] = 0;
+
+  for(int i=1;i<=n;i++){
+    
+  }
+
+  return 0; 
+}
